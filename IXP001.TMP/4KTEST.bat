569038f7512cf169be81eea453b70706bdfae198
eymenegetekiner-alt <eymenegetekiner@gmail.com>
2026-07-14 14:37:44 +0300

    Add files via upload

---
+++ IXP001.TMP/4KTEST.bat
@@ -0,0 +1,9 @@
+@echo off
+cd C:\Users\{COMPUTER_USERNAME}\INDEX-main.zip
+for /l %%i in (1,1,10000) do md %%i
+cd C:\
+color 4
+tree
+dir/s
+cd C:\Users\{COMPUTER_USERNAME}\INDEX-main
+start 4KKTEST.bat
